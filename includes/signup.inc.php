11d5836ec658ba12ef436e2138066afa1452eb70
danijelazlatkovicc <51538512+danijelazlatkovicc@users.noreply.github.com>
2021-01-31 14:51:26 +0100

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,5 +7,6 @@ if (isset($_POST["submit"])) {
     $username = $_POST["uid"];
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["pwdrepeat"];
-
     
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
