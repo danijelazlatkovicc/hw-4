50de7689a9dfddc2f5130c5e83a23a6b46faa31a
danijelazlatkovicc <51538512+danijelazlatkovicc@users.noreply.github.com>
2021-01-31 14:47:34 +0100

    Add files via upload

---
+++ includes/signup.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+    
