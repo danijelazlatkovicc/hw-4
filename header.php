d940a5f3ec25247987860579ac4f2dbc57b41e11
danijelazlatkovicc <51538512+danijelazlatkovicc@users.noreply.github.com>
2021-01-31 14:12:38 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.php">
+    <title>Homepage</title>
+</head>
+<body>
+    
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/imdb-logo.png" alt="logo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="movies.php">Movies</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once 'header.php';
+?>
+        
+        <h1>Dobrodosli durgovi</h1>
+    
+    
+    
+    
+<?php 
+    include_once 'footer.php';
+?>
