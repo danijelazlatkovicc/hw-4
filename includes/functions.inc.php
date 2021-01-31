d0a7fd2f42f9088046d292832ee91008b925fa25
danijelazlatkovicc <51538512+danijelazlatkovicc@users.noreply.github.com>
2021-01-31 22:41:29 +0100

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -33,6 +33,25 @@ function invalidEmail($email){
     return $result;
 }
 
+function deleteMovie($conn, $title){
+    $sql = "DELETE FROM movies WHERE moviesTitle = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../movies-delete.php?error=stmtfailed");
+        exit();
+    }
+    
+    mysqli_stmt_bind_param($stmt, "s", $title);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header("location: ../movies-delete.php?error=none");
+    exit();
+}
+
+
+
 function pwdMatch($pwd, $pwdRepeat){
     $result;
     if($pwd !== $pwdRepeat){
