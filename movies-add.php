6c03150dda2eb69bdc63fa89eaf9ec0f8bdad77a
danijelazlatkovicc <51538512+danijelazlatkovicc@users.noreply.github.com>
2021-01-31 20:28:35 +0100

    Update movies-add.php

--- movies-add.php
+++ movies-add.php
@@ -1,6 +1,39 @@
 <?php
-    include_once 'header.php';
+    session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Add movie</title>
+</head>
+<body>
+    
+    <nav>
+        <div class="wrapper">
+            <!--<a href="index.php"><img src="img/imdb-logo.png" alt="logo"></a>-->
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="movies-admin.php">Movies</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <?php
+                    if(isset($_SESSION["useruid"])){
+                        echo "<li><a href='watchlist.php'>Watchlist</a></li>";
+                        echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
+                    }
+                    else {
+                        echo "<li><a href='signup.php'>Sign Up</a></li>";
+                        echo "<li><a href='login.php'>Log In</a></li>";
+                    }
+                ?>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
         
     <section class="movies-form">
         <h2>Add movie</h2>
